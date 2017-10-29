a1b8122700800cc6dbefa91f479e59a7aee547f6
Amruta Amit Ghodke <AmrutaAGhodke@Amrutas-MacBook-Air.local>
2017-10-28 20:27:35 -0500

    Exception added to LengthOfString

--- LengthOfString.py
+++ LengthOfString.py
@@ -1,5 +1,11 @@
 def lengthofstring(somestring):
-    output=len(somestring)
-    return(output)
+    try:
+        output=len(somestring)
+        return(output)
+    except TypeError:
+        output=0
+        print("Length function cannot be called on Integers")
+        return(output)
 
 print(lengthofstring("Mary Had a Little Lamb"))
+print(lengthofstring(10))
